bff7a5232dcf12d8aa94cc60f6c661c6fee26a3c
07alizaa <simkhadaaliza080@gmail.com>
2024-12-14 23:23:32 +0545

    Modified Home Page

--- lib/module/HomePage.dart
+++ lib/module/HomePage.dart
@@ -40,99 +40,102 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF0B3948),
-      body: Center(
-        child: FadeTransition(
-          opacity: _fadeInAnimation,
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 20),
-                const Text(
-                  'STOCK DATA HUB',
-                  style: TextStyle(
-                    color: Colors.orange,
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 8),
-                const Text(
-                  'Optimizing Inventory, Maximizing Sales',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(height: 40),
-                ScaleTransition(
-                  scale: _imageAnimation,
-                  child: SizedBox(
-                    height: 200,
-                    child: Image.asset(
-                      'assets/logo.png', // Add your illustration here
-                      fit: BoxFit.contain,
+        backgroundColor: const Color(0xFF0B3948),
+        body: Center(
+            child: FadeTransition(
+              opacity: _fadeInAnimation,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 20),
+                    Container(
+                      // decoration: BoxDecoration(
+                      //   shape: BoxShape.circle,
+                      //   border: Border.all(color: Colors.orange, width: 2),
+                      // ),
+                      padding: const EdgeInsets.all(20),
+                      child: const Text(
+                        '"WELCOME TO STOCK DATA HUB"',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.orange,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 40),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const LoginPage()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.orange,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
+                    const SizedBox(height: 8),
+                    const Text(
+                      'Optimizing Inventory & Maximizing Sales',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
                     ),
-                    padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 16),
-                  ),
-                  child: const SizedBox(
-                    width: 200, // Adjust the width to your desired size
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Get Started',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
+                    const SizedBox(height: 40),
+                    ScaleTransition(
+                      scale: _imageAnimation,
+                      child: ClipOval(
+                        child: SizedBox(
+                          height: 150,
+                          width: 150,
+                          child: Image.asset(
+                            'assets/logo.png',
+                            fit: BoxFit.cover,
+                            errorBuilder: (context, error, stackTrace) => Container(
+                              color: Colors.grey,
+                              child: const Icon(
+                                Icons.image,
+                                color: Colors.white,
+                                size: 50,
+                              ),
+                            ),
                           ),
                         ),
-                        SizedBox(width: 8), // Add space between text and icon
-                        Icon(Icons.arrow_forward, color: Colors.white),
-                      ],
+                      ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const LoginPage()),
-                    );
-                  },
-                  child: const Text(
-                    'Go to login page',
-                    style: TextStyle(
-                      color: Colors.orange,
-                      fontSize: 16,
+                    const SizedBox(height: 40),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const LoginPage()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 16),
+                      ),
+                      child: const SizedBox(
+                        width: 200, // Adjust the width to your desired size
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Go to Login Page',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                            SizedBox(width: 8), // Add space between text and icon
+                            Icon(Icons.arrow_forward, color: Colors.white),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
-    );
-  }
+            ),
+        );
+    }
 }
